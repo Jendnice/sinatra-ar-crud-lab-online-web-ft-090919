2e78349ebe3490279e9dfe7d4ad73e2bb93d14a1
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-27 20:55:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do 
     @article = Article.create(params[:article])
+    binding.pry 
     
     redirect "/articles/:id"
   end 
